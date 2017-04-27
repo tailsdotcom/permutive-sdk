70ef0764d2cf24041ea898679cad9f4e5a24b654
Dinesh Vitharanage <dvitharanage@gmail.com>
2017-04-27 18:28:24 +0100

    Bump version: 0.5.0 → 0.6.0

--- permutive/__init__.py
+++ permutive/__init__.py
@@ -5,7 +5,7 @@ from .user import UserResource
 
 __author__ = """Dinesh Vitharanage"""
 __email__ = 'dvitharanage@gmail.com'
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 
 class Permutive(object):
